33f223dbcc05774a5b97f42d1247acc2d809686a
Tim Hall <timothypatrickhall@gmail.com>
2016-12-07 21:32:12 +0000

    Create homepage spec

---
+++ spec/features/creating_homepage_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.feature "Creating the Homepage" do
+   scenario do
+       visit '/'
+    expect(page).to have_link("Home")
+    expect(page).to have_link("Athletes Den")
+    expect(page).to have_content("Workout Lounge!")
+    expect(page).to have_content("Show off your workout") 
+   end
+end
