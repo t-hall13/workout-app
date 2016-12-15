518a742cbfd27ee9a15f1b42ec407aaa83b21005
Tim Hall <timothypatrickhall@gmail.com>
2016-12-15 22:14:03 +0000

    User sign up and sign in specs

--- Guardfile
+++ Guardfile
@@ -56,6 +56,8 @@ guard :rspec, cmd: "bin/rspec" do
   watch(%r{^app/models/(.+)\.rb$}) { |m| "spec/features/#{m[1]}s" }
   watch(%r{^app/controllers/(.+)_(controller)\.rb$}) { |m| "spec/features/#{m[1]}" }
   watch(rails.routes) { "#{rspec.spec_dir}" }
+  watch(%r{^app/views/layouts/application.html.erb$}) { "spec/features" }
+
 
   # Rails config changes
   watch(rails.spec_helper)     { rspec.spec_dir }
--- spec/features/users/signing_up_users_spec.rb
+++ spec/features/users/signing_up_users_spec.rb
@@ -10,21 +10,6 @@ RSpec.feature 'Sign up users' do
        click_button "Sign up"
        
        expect(page).to have_content("You have successfully signed up")
-       
     end
-    
-    scenario "with invalid credentials" do
-       visit '/'
-       click_link "Sign up"
-       fill_in "Email", with: ""
-       fill_in "Password", with: ""
-       fill_in "Password confirmation", with: ""
-       click_button "Sign up"
-       
-       expect(page).to have_content("You have not successfully signed up")
-       
-    end
-    
-    
-    
+
 end
