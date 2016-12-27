de51ed6cbbd7724c18a5c4b9c4dc3b59863090d5
Tim Hall <timothypatrickhall@gmail.com>
2016-12-27 22:11:34 +0000

    Write creating exercise with invalid inputs feature

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,6 @@
 class Exercise < ApplicationRecord
+  validates :duration_in_minutes, numericality: { only_integer: true }
+  validates :workout, presence: true
+  validates :workout_date, presence: true
   belongs_to :user
 end
