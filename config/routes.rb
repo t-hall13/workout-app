275561bcb801fdff05201fe20397af836f7f436b
Tim Hall <timothypatrickhall@gmail.com>
2017-01-05 21:29:34 +0000

    Add first and last names to signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'dashboards#index'
   
   resources :users do
-      resources :exercises
+      resources :exercises 
   end
 end
--- spec/features/users/signing_up_users_spec.rb
+++ spec/features/users/signing_up_users_spec.rb
@@ -4,12 +4,17 @@ RSpec.feature 'Sign up users' do
     scenario "with valid credentials" do
        visit '/'
        click_link "Sign up"
-       fill_in "Email", with: "user@example.com"
+       fill_in "First name", with: "John"
+       fill_in "Last name", with: "Doe"
+       fill_in "Email", with: "john@example.com"
        fill_in "Password", with: "password"
        fill_in "Password confirmation", with: "password"
        click_button "Sign up"
        
        expect(page).to have_content("You have signed up successfully")
+       
+       visit '/'
+       expect(page).to have_content("John Doe")
     end
 
 end
