1007f86ca3ae34a4915b5df5f9c2db2cf575d5be
Tim Hall <timothypatrickhall@gmail.com>
2017-01-10 14:59:01 +0000

    Write following friends feature test

---
+++ spec/features/followings/following_friends_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+RSpec.feature "Following Friends" do
+  
+ before do
+  @john = User.create(first_name: "John",
+                       last_name: "Doe",
+                       email: "john@example.com",
+                       password: "password")
+                       
+  @sarah = User.create(first_name: "Sarah",
+                       last_name: "Connor",
+                       email: "sarah@example.com",
+                       password: "password1")
+                       
+  login_as(@john)                     
+  end
+  
+  scenario "if signed in" do
+    visit '/'
+    
+    expect(page).to have_content(@john.full_name)
+    expect(page).to have_content(@sarah.full_name)
+    
+    href = "/friendships?friend_id=#{@john.id}"
+    expect(page).not_to have_link("Follow", :href => href)
+    
+    link = "a[href='/friendships?friend_id=#{@sarah.id}']"
+    find(link).click
+    
+    href = "/friendships?friend_id=#{@sarah.id}"
+    expect(page).not_to have_link("Follow", :href => href)
+  end
+end
